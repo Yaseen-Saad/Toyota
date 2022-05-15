562773454c79dd3a024dc8928676fd907a5458d3
YASOOCODE <yaseensaadeldinmohamed@gmail.com>
2022-05-15 13:55:49 +0200

    first land

---
+++ main-js/main.js
@@ -0,0 +1,17 @@
+function closing_nav(params) {
+  const nav = document.getElementById("side-bar");
+  const closer1 = document.getElementById("nav-close");
+  const closer2 = document.getElementById("nav-closer");
+  nav.style.width = "0px";
+  closer2.style.width = "0%";
+}
+function oppinenig_nav(params) {
+  const nav = document.getElementById("side-bar");
+  const closer1 = document.getElementById("nav-close");
+  const closer2 = document.getElementById("nav-closer");
+  nav.style.width = "250px";
+  closer2.style.width = "100%";
+
+
+
+}
